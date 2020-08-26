aa8af5911c1099c604dfff8b7e9346534e61cf9b
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-26 21:33:16 +0000

    added constants file & added extras message keys

---
+++ constants.js
@@ -0,0 +1,21 @@
+const IP = '135.23.222.131';
+const PORT = 50542;
+
+const MOVE_UP_KEY = 'w';
+const MOVE_LEFT_KEY = 'a';
+const MOVE_DOWN_KEY = 's';
+const MOVE_RIGHT_KEY = 'd'
+const MESSAGES_KEYS = {
+  q: 'SnakeJazz tssstststs',
+  e: 'Breaking News: ...',
+  r: 'Ssslitherin'
+}
+module.exports = {
+  IP,
+  PORT,
+  MOVE_UP_KEY,
+  MOVE_LEFT_KEY,
+  MOVE_DOWN_KEY,
+  MOVE_RIGHT_KEY,
+  MESSAGES_KEYS
+};
--- input.js
+++ input.js
@@ -1,23 +1,27 @@
+const {MOVE_UP_KEY, MOVE_LEFT_KEY, MOVE_DOWN_KEY, MOVE_RIGHT_KEY, MESSAGES_KEYS} = require('./constants')
 let connection;
 const handleUserInput = function(key) {
   
   switch (key) {
     case '\u0003':
       process.exit();
-    case 'w':
+    case MOVE_UP_KEY:
       connection.write("Move: up")
       break;
-    case 'a':
+    case MOVE_LEFT_KEY:
       connection.write("Move: left");
       break;
-    case 's':
+    case MOVE_DOWN_KEY:
       connection.write("Move: down");
       break;
-    case 'd':
+    case MOVE_RIGHT_KEY:
       connection.write("Move: right");
       break;
-    case 'q':
-      connection.write("Say: SnakeJazz tssstststs");
+    default:
+      const msg = MESSAGES_KEYS[key];
+      if (msg) {
+        connection.write(`Say: ${msg}`)
+      }
       break;
   }
 }

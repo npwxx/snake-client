284ac8575889c7988f2dd1efa7d88608047d7180
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-26 19:51:41 +0000

    added name

--- client.js
+++ client.js
@@ -10,6 +10,10 @@ const connect = function() {
   conn.on("data", (data) => {
     console.log(data)
   })
+  conn.on("connect", () => {
+    console.log("Successfully connected to game server")
+    conn.write("Name: NIC")
+  })
   return conn;
 }
 module.exports = {connect};

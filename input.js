8e0d436b9647823e8f887af7b010f7c77f6fe7f2
Nicole Woodcock <nicole.xox@gmail.com>
2020-08-26 21:10:49 +0000

    refactored to switch statements & added message -snakeJazz tssstststs

--- input.js
+++ input.js
@@ -1,11 +1,28 @@
+let connection;
 const handleUserInput = function(key) {
-  if (key === '\u0003') {
-    process.exit();
-  }
   
-  console.log("key was pressed", key)
+  switch (key) {
+    case '\u0003':
+      process.exit();
+    case 'w':
+      connection.write("Move: up")
+      break;
+    case 'a':
+      connection.write("Move: left");
+      break;
+    case 's':
+      connection.write("Move: down");
+      break;
+    case 'd':
+      connection.write("Move: right");
+      break;
+    case 'q':
+      connection.write("Say: SnakeJazz tssstststs");
+      break;
+  }
 }
-const setupInput = function() {
+const setupInput = function(conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
--- play.js
+++ play.js
@@ -1,5 +1,5 @@
 const { connect } = require('./client');
 const { setupInput } = require('./input')
 console.log('Connecting ...');
-connect();
-setupInput()
+const connection = connect();
+setupInput(connection);
